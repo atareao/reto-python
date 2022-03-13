096a7758813b46c1254d9790d378158d2f2ddd6d
Roberto B <rburgosmartin@gmail.com>
2022-03-13 08:02:59 +0100

    Añadidos archivos main

--- reto-01/rburgos/main.py
+++ reto-01/rburgos/main.py
@@ -2,9 +2,11 @@
 import os
 
 def main():
+
     """Buscar directorio descargar y listar contenido"""
     descargas = os.popen("xdg-user-dir DOWNLOAD").read().strip()
     print("Directorio:", descargas, "\n".join(os.listdir(descargas)))
 
 if __name__ == '__main__':
+
     main()
--- reto-02/rburgos/main.py
+++ reto-02/rburgos/main.py
@@ -2,24 +2,28 @@
 import os
 
 def buscar_imagenes(archivo):
+
     """Filtrar imágenes jpg y jpeg"""
     return archivo.lower().endswith(('jpg', 'jpeg'))
 
 def check_numero(string):
+
     """Comprobar si el nombre tiene un número"""
     return string.lower() if any(map(str.isdigit, string)) else string.upper()
 
 def main():
+
     """Listar las imágenes dentro de descargas"""
     descargas = os.popen("xdg-user-dir DOWNLOAD").read().strip()
 
     archivos_jpg = filter(buscar_imagenes, os.listdir(descargas))
 
     for idx, val in enumerate(map(check_numero, archivos_jpg)):
-        if idx%2:
+        if idx % 2:
             print(idx, val)
         else:
             print('{} => "{}"'.format(idx, val))
 
 if __name__ == '__main__':
+
     main()
