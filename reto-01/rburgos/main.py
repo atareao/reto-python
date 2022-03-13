bb9fb02be4b8094e36586a4adcd283a89f454a51
Roberto B <rburgosmartin@gmail.com>
2022-03-13 07:42:43 +0100

    Añadidos retos 01 y 02

---
+++ reto-01/rburgos/main.py
@@ -0,0 +1,10 @@
+"""Reto 01 Python Atareao"""
+import os
+
+def main():
+    """Buscar directorio descargar y listar contenido"""
+    descargas = os.popen("xdg-user-dir DOWNLOAD").read().strip()
+    print("Directorio:", descargas, "\n".join(os.listdir(descargas)))
+
+if __name__ == '__main__':
+    main()
