0cc8ffed3a4b6c13ac719e8fa5b7964670491422
Roberto B <rburgosmartin@gmail.com>
2022-03-19 09:31:34 +0100

    Planteamiento reto03

---
+++ reto-03/rburgos/main.py
@@ -0,0 +1,40 @@
+# El usuario define el directorio que quira usar para listar
+# Guardar el directorio en un archivo de conf
+import os
+
+def buscar_imagenes(archivo):
+    """Filtrar imágenes jpg y jpeg"""
+    return archivo.lower().endswith(('jpg', 'jpeg'))
+
+def check_numero(string):
+    """Comprobar si el nombre tiene un número"""
+    return string.lower() if any(map(str.isdigit, string)) else string.upper()
+
+def main():
+    conf_path = "/home/roberto/.config/diogenes/"
+    conf_file = "diogenes.conf"
+    conf_completo = conf_path + conf_file
+
+    try:
+        if not os.path.exists(conf_completo):
+            os.mkdir(conf_path)
+            with open(conf_completo, 'w') as f:
+                descargas = os.popen("xdg-user-dir DOWNLOAD").read().strip()
+                f.write(f'directorio = "{descargas}"')
+        configuracion = open(conf_completo).readline()
+        directorio = configuracion[configuracion.index("=") +1 :].strip().replace('"','')
+
+
+        if not os.path.exists(directorio):
+            raise Exception(f'No existe el directorio {directorio}')
+
+        archivos_jpg = filter(buscar_imagenes, os.listdir(directorio))
+
+        for idx, val in enumerate(map(check_numero, archivos_jpg)):
+            print(idx, val) if idx % 2 else print('{} => "{}"'.format(idx, val))
+
+    except Exception as exception:
+        print(exception)
+
+if __name__ == '__main__':
+    main()
