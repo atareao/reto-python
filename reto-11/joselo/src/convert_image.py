c25146f870b8b46686aef8235cb1a96ca5679976
José Lorenzo Nieto Corral <joselo.prbg@gmail.com>
2022-06-03 00:34:20 +0200

    Primer borrador del Reto 11.

---
+++ reto-11/joselo/src/convert_image.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Reto 10: clase Convert."""
+
+# Copyright (c) 2022 José Lorenzo Nieto Corral <a.k.a. jlnc> <a.k.a. JoseLo>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from pathlib import Path
+from PIL import Image
+
+
+class Convert:
+    """Convert."""
+
+    def __init__(self, filein: Path, fileout: Path) -> None:
+        self.__filein = Path(filein)
+        self.__fileout = Path(fileout)
+
+    def check(self) -> bool:
+        """check."""
+        return self.__filein.is_file() \
+            and not self.__filein.is_symlink() \
+            and self.__filein.suffix in ('.jpg', '.png', '.bmp') \
+            and self.__fileout.parent.is_dir() \
+            and not self.__fileout.exists() \
+            and self.__fileout.suffix in ('.jpg', '.png', '.bmp', '.pdf')
+
+    def execute(self) -> None:
+        """execute."""
+        with Image.open(self.__filein) as image:
+            if self.__filein.suffix == '.png':
+                image = image.convert('RGB')
+            image.save(self.__fileout)
+
+
+def main():  # noqa
+    filein = Path("/home/lorenzo/kk/bb.jpg")
+    fileout = Path("/home/lorenzo/kk/bb.pdf")
+    action = Convert(filein, fileout)
+    if action.check():
+        action.execute()
+
+
+if __name__ == '__main__':
+    main()
+    # test()
