bad7d42ccbeed9a290e7677293919975eeccc357
rcccort <rcccort@hotmail.com>
2022-06-16 09:35:29 +0200

    cambios super pc

--- reto-09/Palypalillo/main.py
+++ reto-09/Palypalillo/main.py
@@ -2,10 +2,15 @@ from image_utils import ImageUtils
 
 def main():
     imagein = '/home/paly/Pictures/avatar.jpg'
-    imageout = '/home/paly/Pictures/grey_avatar.jpg'
-    imagen = ImageUtils(imagein, imageout)
-    if imagen.check():
-        imagen.greyscale_image()
+    imageout = '/home/paly/Pictures/modifi_avatar.jpg'
+    args = {"w": 140,"h": 200}
+    modificacion = ImageUtils(imagein, imageout, args)
+
+    if modificacion.check():
+        modificacion.resize_image()
+        modificacion2 = ImageUtils(imageout, imageout)
+        if modificacion2.check():
+            modificacion2.greyscale_image()
 
 if __name__ == "__main__":
     main()
